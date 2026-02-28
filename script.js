663dc9413dcbb85d59e94cf3196ecb7fc7506466
giovannipastorini <giovannipastorini9@gmail.com>
2026-02-28 09:22:50 +0100

    index.html: eliminate bootstrap grid system and form

---
+++ script.js
@@ -0,0 +1,16 @@
+/* 
+Calcolare il prezzo totale del viaggio, secondo queste regole:
+
+1)il prezzo del biglietto è definito in base ai km (0.21 € al km),
+2)va applicato uno sconto del 20% per i minorenni,
+3)va applicato uno sconto del 40% per gli over 65.,
+4)L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). 
+Questo richiederà un minimo di ricerca.
+
+*/
+
+/* 
+1)il prezzo del biglietto è definito in base ai km (0.21 € al km),
+- Crea una costante di tipo numerica decimale con valore 0.21
+*/
+
